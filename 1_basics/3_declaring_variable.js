dcabc53b9a04e03d055e007cf3471ad10dd53c32
juan-rybczinski <jhreplay.lee@gmail.com>
2023-12-21 04:49:11 +0900

    변수 선언하기

---
+++ 1_basics/3_declaring_variable.js
@@ -0,0 +1,38 @@
+/**
+ * Variable 선언하기
+ *
+ * 1) var - 더 이상 쓰지 않는다.
+ * 2) let
+ * 3) const
+ */
+var name = '코드팩토리';
+console.log(name);
+
+var age = 32;
+console.log(age);
+
+let ive = '아이브';
+console.log(ive);
+
+/**
+ * let과 var로 선언하면
+ * 값을 ㅜ후에 변경할 수 있다.
+ */
+ive = '안유진';
+console.log(ive);
+
+const newJeans = '뉴진스';
+console.log(newJeans);
+
+// newJeans = '코드팩토리';
+
+/**
+ * 선언: 변수의 선언
+ * 할당: 값의 할당
+ */
+var name = '코드팩토리';
+
+let girlFriend;
+console.log(girlFriend);
+
+// const girlFriend2;
